7948352cc10dade0b668b7c70ab9d735fadf0c74
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2023-12-08 12:28:38 +0100

    WIP

--- src/components/Functions/PlaySound.vue
+++ src/components/Functions/PlaySound.vue
@@ -23,12 +23,21 @@
 </template>
 
 <script>
+import { useSoundManager } from '../../Use/useSoundManager';
+
 export default {
     props: {
         plugin: { type: Object, required: true },
         args: { type: Object, required: true },
     },
     emits: ['update:args'],
+    setup(props) {
+        const { sounds } = useSoundManager(props.plugin.id);
+
+        return {
+            sounds,
+        };
+    },
     computed: {
         id() {
             return this.args.id;
@@ -37,9 +46,9 @@ export default {
             return this.args.options;
         },
         soundInstancesOptions() {
-            console.log('TOTO', this.plugin.sounds);
+            console.log('TOTO', this);
 
-            return Object.values(this.plugin.sounds.value).map(sound => ({
+            return Object.values(this.sounds.value).map(sound => ({
                 label: sound.label,
                 value: sound.id,
             }));
