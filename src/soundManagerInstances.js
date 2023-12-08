e19bcf05a25157bed749777cdb66ed36a536d34a
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2023-12-08 16:37:12 +0100

    test

--- src/soundManagerInstances.js
+++ src/soundManagerInstances.js
@@ -29,7 +29,7 @@ function createSoundManager(pluginId) {
                 onseek: () => updateSoundProperties(id),
             });
 
-            // soundInstance.play();
+            soundInstance.play();
         });
     };
 
