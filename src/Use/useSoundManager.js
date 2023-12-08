c545c59d35934156f34f901168d64f27f98d56fc
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2023-12-08 12:50:57 +0100

    wip

--- src/Use/useSoundManager.js
+++ src/Use/useSoundManager.js
@@ -25,7 +25,6 @@ export function useSoundManager(pluginId) {
             onend: () => updateSoundProperties(id),
             onpause: () => updateSoundProperties(id),
             onstop: () => updateSoundProperties(id),
-            // Add other relevant events here
         });
 
         return id;
