8fa503e9f28ed4a3cae82f0b3d67d62b62a56901
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2023-12-11 15:35:04 +0100

    wip

--- ww-config.js
+++ ww-config.js
@@ -66,17 +66,17 @@ export default {
             },
             /* wwEditor:end */
         },
-        // {
-        //     name: 'Seek to time',
-        //     code: 'seekTo',
-        //     isAsync: true,
-        //     /* wwEditor:start */
-        //     edit: () => import('./src/components/Functions/SeekTo.vue'),
-        //     getIsValid({ id, position }) {
-        //         return !!id && position != null;
-        //     },
-        //     /* wwEditor:end */
-        // },
+        {
+            name: 'Seek to time',
+            code: 'seekTo',
+            isAsync: true,
+            /* wwEditor:start */
+            edit: () => import('./src/components/Functions/SeekTo.vue'),
+            getIsValid({ id, position }) {
+                return !!id && position != null;
+            },
+            /* wwEditor:end */
+        },
         // {
         //     name: 'Set volume',
         //     code: 'setVolume',
