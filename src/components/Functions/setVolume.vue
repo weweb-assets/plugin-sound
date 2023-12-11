3648340fa41ceb5edf1bade28c776943ba8adb64
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2023-12-11 16:00:08 +0100

    wip

--- src/components/Functions/setVolume.vue
+++ src/components/Functions/setVolume.vue
@@ -24,47 +24,45 @@
 </template>
 
 <script>
-import { computed } from 'vue';
-import { getSoundManagerInstance } from '../../soundManagerInstances';
+// import { computed } from 'vue';
+// import { getSoundManagerInstance } from '../../soundManagerInstances';
 
 export default {
-    props: {
-        plugin: { type: Object, required: true },
-        args: { type: Object, required: true },
-    },
-    emits: ['update:args'],
-    setup(props) {
-        const { sounds } = getSoundManagerInstance(props.plugin.id);
-
-        const soundOptions = computed(() =>
-            Object.values(sounds.value).map(sound => ({
-                label: sound.id,
-                value: sound.id,
-            }))
-        );
-
-        return { soundOptions };
-    },
-    data() {
-        return {
-            volumeHelp: `The volume is a number between 0 and 1. 0 is silent, 0.5 is half volume, 1 is full volume.`,
-        };
-    },
-    computed: {
-        id() {
-            return this.args.id;
-        },
-        volume() {
-            return this.args.volume;
-        },
-    },
-    methods: {
-        setId(id) {
-            this.$emit('update:args', { ...this.args, id });
-        },
-        setVolume(volume) {
-            this.$emit('update:args', { ...this.args, volume });
-        },
-    },
+    // props: {
+    //     plugin: { type: Object, required: true },
+    //     args: { type: Object, required: true },
+    // },
+    // emits: ['update:args'],
+    // setup(props) {
+    //     const { sounds } = getSoundManagerInstance(props.plugin.id);
+    //     const soundOptions = computed(() =>
+    //         Object.values(sounds.value).map(sound => ({
+    //             label: sound.id,
+    //             value: sound.id,
+    //         }))
+    //     );
+    //     return { soundOptions };
+    // },
+    // data() {
+    //     return {
+    //         volumeHelp: `The volume is a number between 0 and 1. 0 is silent, 0.5 is half volume, 1 is full volume.`,
+    //     };
+    // },
+    // computed: {
+    //     id() {
+    //         return this.args.id;
+    //     },
+    //     volume() {
+    //         return this.args.volume;
+    //     },
+    // },
+    // methods: {
+    //     setId(id) {
+    //         this.$emit('update:args', { ...this.args, id });
+    //     },
+    //     setVolume(volume) {
+    //         this.$emit('update:args', { ...this.args, volume });
+    //     },
+    // },
 };
 </script>
