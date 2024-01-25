bb29a5f10773023a6ef300c54ecbfb54470c09eb
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2024-01-25 14:03:19 +0100

    Test

--- src/soundManagerInstances.js
+++ src/soundManagerInstances.js
@@ -103,6 +103,7 @@ function createSoundManager(pluginId) {
         const sound = soundInstances[id];
 
         if (sound) {
+            console.log('seeking to', time, Number(time));
             sound.seek(Number(time));
             updateSoundProperties(id);
         }
