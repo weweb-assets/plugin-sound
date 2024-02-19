f31628121006d21831b8f7885d5211f39158cfee
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2024-02-19 03:07:33 +0100

    test

---
+++ src/components/Configuration/SettingsEdit.vue
@@ -0,0 +1,18 @@
+<template>
+    <wwEditorFormRow required label="Sound"> </wwEditorFormRow>
+</template>
+
+<script>
+export default {
+    props: {
+        plugin: { type: Object, required: true },
+        settings: { type: Object, required: true },
+    },
+    emits: ['update:settings'],
+    data() {
+        return {};
+    },
+    mounted() {},
+    methods: {},
+};
+</script>
--- ww-config.js
+++ ww-config.js
@@ -1,5 +1,14 @@
 export default {
-    editor: {},
+    editor: {
+        settings: [
+            {
+                label: 'Configuration',
+                icon: 'advanced',
+                edit: () => import('./src/components/Configuration/SettingsEdit.vue'),
+                summary: () => import('./src/components/Configuration/SettingsSummary.vue'),
+            },
+        ],
+    },
     variables: [{ name: 'sounds', value: 'sounds', type: 'object', defaultValue: null }],
     actions: [
         {
